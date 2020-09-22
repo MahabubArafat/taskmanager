bfa02c4717192d7467b8cd3b18771259a9aa76a4
MahabubArafat <arafatmahabub37@gmail.com>
2020-09-22 18:36:30 +0600

    task manager

---
+++ app.js
@@ -0,0 +1,5 @@
+const clearBtn = document.querySelector(".clear-tasks");
+const taskInput = document.querySelector("#task");
+const form = document.querySelector("#task-form");
+const filter = document.querySelector("#filter");
+const taskList = document.querySelector(".collection");
